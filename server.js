1e97fabaab6cf21408197f795ded564c4fa32c03
Tarun Kumar <146514355+codebyTarun08@users.noreply.github.com>
2025-11-19 12:41:37 +0530

    Update CORS origin to include production URL

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
   pingTimeout: 60000,
   pingInterval: 25000,
   cors: {
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["https://trust-consult.vercel.app",http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
