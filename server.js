b053a30ebeaf74eb44bf9cc0746bda588399407e
Tarun Kumar <146514355+codebyTarun08@users.noreply.github.com>
2025-11-19 12:44:33 +0530

    Fix syntax error in CORS origin array

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
   pingTimeout: 60000,
   pingInterval: 25000,
   cors: {
-    origin: ["https://trust-consult.vercel.app",http://localhost:3000"],
+    origin: ["https://trust-consult.vercel.app","http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
